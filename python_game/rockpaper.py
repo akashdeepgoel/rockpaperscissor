b4658a12922d66b98db9b4005f57d5bcc512466b
Akashdeep Goel <akashdeep.goel1996@gmail.com>
2015-05-28 00:01:36 +0530

    Restructured the code

--- python_game/rockpaper.py
+++ python_game/rockpaper.py
@@ -1,15 +1,42 @@
 #First Python Game
 #Rock Paper Scissor - Implements Random number using Python standard random number generator
 from random import *
+import os
 def choose():
-	chosen = randint(0,2)
-	if chosen == 0:
-		return "Rock"
-	elif chosen == 1:
-		return "Paper"
-	else:
-		return "Scissor"
+	chosen = randint(1,3)
+	return chosen
 
+def findwinner(user_choice,pc_choice):
+	win = "You Won!!\n"
+	lose = "You Lose..I guess You are Unlucky\n"
+	tie = "It is a tie!!\n"
+	out = "PC Chose %s\n"
+	choice = ""
+	if pc_choice==1:
+		choice+="Rock"
+	elif pc_choice==2:
+		choice+="Paper"
+	else:	
+		choice+="Scissor"
+	if user_choice==pc_choice:
+		print out%(choice)
+		print tie
+	elif user_choice>pc_choice:
+		diff = user_choice - pc_choice
+		if diff==1:
+			print out%(choice)
+			print win
+		else:
+			print out%(choice)
+			print lose
+	else:
+		diff = pc_choice - user_choice
+		if diff==1:
+			print out%(choice)
+			print lose
+		else:
+			print out%(choice)
+			print win
 
 def begin():
 	print "Ok Let's Play!"
@@ -17,60 +44,19 @@ def begin():
 	return choice
 
 def play():
+	clear = lambda: os.system('clear')
+	clear()
 	user_choice = begin()
 	pc_choice = choose()
-	if pc_choice=="Rock":
-		if user_choice==2:
-			print "PC Chose Rock\n"
-			print "You Won!!\n"
-		elif user_choice==1:
-			print "PC Chose Rock\n"
-			print "It is a tie!!\n"
-		else:
-			print "PC Chose Rock\n"
-			print "You Lose..I guess You are Unlucky\n"
+	findwinner(user_choice,pc_choice)
+	cont = input("Would You Like To Continue? (1 for Yes and 0 for No)")
+	if cont==1:
 
-		cont = input("Would You Like To Continue? (1 for Yes and 0 for No)")
-		if cont==1:
-			play()
-		else:
-			print "Hope you enjoyed!!"
-			exit(0)
-	if pc_choice=="Scissor":
-		if user_choice==2:
-			print "PC Chose Scissor\n"
-			print "You Lose..I guess You are Unlucky\n"
-		elif user_choice==1:
-			print "PC Chose Scissor\n"
-			print "You Won!!\n"
-		else:
-			print "PC Chose Scissor\n"
-			print "It is a tie!!\n"
-
-		cont = input("Would You Like To Continue? (1 for Yes and 0 for No)")
-		if cont==1:
-			play()
-		else:
-			print "Hope you enjoyed!!"
-			exit(0)
-	if pc_choice=="Paper":
-		if user_choice==2:
-			print "PC Chose Paper\n"
-			print "It is a tie!!\n"
-		elif user_choice==1:
-			print "PC Chose Paper\n"
-			print "You Lose..I guess You are Unlucky\n"
-		else:
-			print "PC Chose Paper\n"
-			print "You Won!!\n"
-
-		cont = input("Would You Like To Continue? (1 for Yes and 0 for No)")
-		if cont==1:
-			play()
-		else:
-			print "Hope you enjoyed!!"
-			exit(0)
-
-
+		play()
+	else:
+		print "Hope you enjoyed!!"
+		exit(0)
+clear = lambda: os.system('clear')
+clear()
 print "Welcome To Rock,Paper Scissor!!!"
 play()
